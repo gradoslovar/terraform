e683dc85c39b415bbb716254e4e9638c5bf05c09
nenad.stojanovic <nenad.stojanovic@outlook.com>
2017-06-30 16:47:33 +0200

    tf init

---
+++ UbuntuVM/variables.tf
@@ -0,0 +1,23 @@
+variable "azurerm_location" {
+  type    = "string"
+  default = "West Europe"
+}
+variable "azurerm_client_id" {
+  type = "string"
+  default = "73be97d0-34ea-4edc-b2ca-3f51a33d73e1"
+}
+
+variable "azurerm_client_secret" {
+  type = "string"
+  default = "DNm11i22-dt3ft"
+}
+
+variable "azurerm_subscription_id" {
+  type = "string"
+  default = "07dd3def-975e-4933-8a0b-5baf4fd2b88f"
+}
+
+variable "azurerm_tenant_id" {
+  type = "string"
+  default = "f846780e-1f26-4cfe-a174-6d6b3da2dafe"
+}
