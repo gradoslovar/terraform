95bdf5c64bd54734eca22cae1738e606274d732d
gradoslovar <gradoslovar@outlook.com>
2019-03-31 20:40:50 +0200

    Further changes to Set-FileShare script

--- Azure/windows-custom-script-provision/scripts/Set-FileShare.ps1
+++ Azure/windows-custom-script-provision/scripts/Set-FileShare.ps1
@@ -6,21 +6,27 @@ param(
     [parameter(Mandatory=$true)]
     [string]$storageKey,
 
-    [parameter(Mandatory=$false)]
-    [string]$fileShareName = "fileshare",
+    [parameter(Mandatory=$true)]
+    [string]$fileShareName,
 
     [parameter(Mandatory=$false)]
-    [string]$fileShareDriveLetter = "S"
+    [string]$fileShareDriveLetter = "S",
+
+    [parameter(Mandatory=$false)]
+    [string]$vmFolder
 )
 
 $fileShareEndpoint = [string]::Concat($storageName, ".file.core.windows.net")
 $fileShareRoot = [string]::Concat("\\",$fileShareEndpoint, "\", $fileShareName)
-if (!(test-path ([string]::Concat("C:\\fileshare\", $fileShareName)))) { 
-    New-Item -Path ([string]::Concat("C:\\fileshare\", $fileShareName)) -ItemType Directory
+if ($vmFolder) {
+    New-Item -Path $vmFolder -ItemType Directory -Force
+    $fileShareFolder = [string]::Concat($vmFolder, "\", $fileShareName)
+} else {
+    $fileShareFolder = [string]::Concat("C:\", $fileShareName)
 }
 
 cmdkey /add:$fileShareEndpoint /user:$storageName /pass:$storageKey
 
 New-PSDrive -Name $fileShareDriveLetter -PSProvider FileSystem -Root $fileShareRoot -Persist
 
-cmd /c "mklink /D $fileShareData $fileShareRoot"
+cmd /c "mklink /D $fileShareFolder $fileShareRoot"
