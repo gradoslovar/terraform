af89c6eee3f6be878d44587d8485702439464ef0
gradoslovar <gradoslovar@outlook.com>
2019-03-31 15:59:44 +0200

    Add Set-FileShare script

--- Azure/windows-custom-script-provision/scripts/Set-FileShare.ps1
+++ Azure/windows-custom-script-provision/scripts/Set-FileShare.ps1
@@ -12,8 +12,10 @@ param(
 
 $fileShareEndpoint = [string]::Concat($storageName, ".file.core.windows.net")
 $fileShareRoot = [string]::Concat("\\",$fileShareEndpoint, "\", $fileShare)
+$fileShareData = [string]::Concat("C:\\fileshare\", $fileShare)
+
 cmdkey /add:$fileShareEndpoint /user:$storageName /pass:$storageKey
 
 New-PSDrive -Name S -PSProvider FileSystem -Root $fileShareRoot -Persist
 
-cmd /c "mklink /D $cetiSpawnerData $fileShareRoot"
+cmd /c "mklink /D $fileShareData $fileShareRoot"
