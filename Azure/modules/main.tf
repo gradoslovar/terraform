f7e2ad70d5b5b37a7d8b2b2478c77e7736763da5
gradoslovar <gradoslovar@outlook.com>
2020-02-06 17:11:18 +0100

    Update network module.

--- Azure/modules/main.tf
+++ Azure/modules/main.tf
@@ -1,29 +1,42 @@
 # declare variables and defaults
 variable "prefix" {}
-variable "environment" {}
 variable "location" {}
 variable "address_space" {}
-variable "subnet_names" {}
+# variable "subnet_names" {}
 variable "subnet_prefixes" {}
 
+# Set Azure provider
+provider "azurerm" {
+  version = "~>1.40.0"
+}
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "${var.prefix}-afad-${var.environment}"
+    name     = "${var.prefix}-afad"
     location = var.location
 }
 
 # Use the network module to create a vnet and subnet
 module "network" {
     source              = "./modules/network"
-    resource_group_name = "${var.prefix}-afad-${var.environment}"
+    resource_group_name = "${var.prefix}-afad"
     location            = var.location
-    vnet_name           = "${var.prefix}-afad-${var.environment}-vnet"
+    vnet_name           = "${var.prefix}-afad-vnet"
     address_space       = var.address_space
-    subnet_names        = var.subnet_names
+    # subnet_names        = var.subnet_names
+    subnet_names        = [
+        "${var.prefix}-sen-subnet",
+        "${var.prefix}-dl-subnet",
+        "${var.prefix}-sql-subnet",
+        "GatewaySubnet"
+    ]
+    # subnet_names        = [
+    #     "sen-subnet",
+    #     "dl-subnet",
+    #     "sql-subnet"
+    # ]
     subnet_prefixes     = var.subnet_prefixes
     tags                = {
-        environment  = var.environment
         customer     = var.prefix
     }
 }
