89648e6e67fb82160cc54648f25fca1759d8fb00
gradoslovar <gradoslovar@outlook.com>
2020-02-06 18:57:18 +0100

    Small RG name change

--- Azure/modules/main.tf
+++ Azure/modules/main.tf
@@ -12,16 +12,16 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-    name     = "${var.prefix}-afad"
+    name     = "${var.prefix}-rg"
     location = var.location
 }
 
 # Use the network module to create a vnet and subnet
 module "network" {
-    source              = "./modules/network"
-    resource_group_name = "${var.prefix}-afad"
+    source              = "./network"
+    resource_group_name = azurerm_resource_group.rg.name
     location            = var.location
-    vnet_name           = "${var.prefix}-afad-vnet"
+    vnet_name           = "${var.prefix}-vnet"
     address_space       = var.address_space
     # subnet_names        = var.subnet_names
     subnet_names        = [
