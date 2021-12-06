9a7c76d57462533325803541f5740b8977acd21f
gradoslovar <gradoslovar@outlook.com>
2021-12-06 23:05:17 +0100

    test

---
+++ Azure/test/variables.tf
@@ -0,0 +1,19 @@
+variable prefix {
+  default = "marta"
+}
+
+variable resource_group_name {
+  default = "marta-afad"
+}
+
+variable location {
+  default = "francecentral"
+}
+
+variable "vnet_address_prefix" {
+  default = "10.200.200.0/24"
+}
+
+variable "subnet_prefix" {
+  default = "10.200.200.0/25"
+}
